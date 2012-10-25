f3a7b88ee0f0fd03c6ae676b1469d3812e3c0e4a
Fernando Ultremare <fernando@dextra-sw.com>
2012-10-25 11:35:10 -0200

    v0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whoshares::Application.config.secret_token = '8e2c6d024e75afffe39513f6e88f70f62ac111ca042f592d17dcb6e2ef526374df59be07b0cec52a2d68aa413d4b13ac65f3310bf385518a1703859dc71ac4ea'
