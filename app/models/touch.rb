adf3792e0c470a7b2c0fcb6074dd8e600acb317b
Fernando Ultremare <feroult@gmail.com>
2012-10-26 21:03:19 -0200

    testapp

--- app/models/touch.rb
+++ app/models/touch.rb
@@ -1,10 +1,18 @@
 class Touch < ActiveRecord::Base
   attr_accessible :from, :touch
-
-  def self.recent_followers(profile)
   
-    
-  
-  end  
+  def self.recent_followers(client, screen_name)
+    cursor = -1
+    begin
+      #followers = client.follower_ids(screen_name, :cursor => cursor)
+      followers = client.friend_ids(screen_name, :cursor => cursor)
+      followers.ids.each do |fid|
+        follower = client.user(fid)
+        p follower.screen_name
+        #p fid
+      end
+      cursor = followers.next_cursor
+    end while cursor != 0
+  end
 
 end
